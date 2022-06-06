c48e6650ab2fd77f1a6d4463b874b74cbc1ac44c
Henriette Steenhoff <henriette.steenhoff@protonmail.com>
2022-06-06 21:33:13 +0200

    Added date check on camppaigns

--- src/components/ForloebSection..svelte
+++ src/components/ForloebSection..svelte
@@ -3,11 +3,22 @@
 	import { client } from '../routes/index.js'
 	
 	let forloeb;
+	let today = new Date().getTime();
 
 	const preload = async () => {
 		const query = "*[_type == 'forloeb']";
 		forloeb = await client.fetch(query);
 	}
+
+	const stringAsDate = (date) => {
+		return new Date(date).getTime();
+	}
+
+
+	const isCampaignStillValid = (startDate, endDate) => {
+		return today > stringAsDate(startDate) && today < stringAsDate(endDate)
+	}
+
 </script>
 
 
@@ -17,34 +28,37 @@
 			<p>Henter data</p>
 		{:then}
 			{#each forloeb as enhed}
-				<div class="row">
-					<div class="col col-xs-12 col-lg-8">
-						<div class="card mb-4">
-							<h5 class="card-header">{enhed.name}</h5>
-							<div class="card-body">
-								{#each enhed.price as pricePair}
-									<div class="row">
-										<div class="col col">
-											<p>{pricePair.description}</p>
+				{#if (!enhed.startDate !== "" || !enhed.endDate !== "") || 
+					(enhed.startDate && enhed.endDate && isCampaignStillValid(enhed.startDate, enhed.endDate))}
+					<div class="row">
+						<div class="col col-xs-12 col-lg-8">
+							<div class="card mb-4">
+								<h5 class="card-header">{enhed.name}</h5>
+								<div class="card-body">
+									{#each enhed.price as pricePair}
+										<div class="row">
+											<div class="col col">
+												<p>{pricePair.description}</p>
+											</div>
+											<div class="col col-auto end">
+												<p>{pricePair.price} {pricePair.currency}</p>
+											</div>
 										</div>
-										<div class="col col-auto end">
-											<p>{pricePair.price} {pricePair.currency}</p>
-										</div>
-									</div>
-								{/each}
-								
-								<i>{enhed.comment ? enhed.comment : ''}</i>
+									{/each}
+									
+									<i>{enhed.comment ? enhed.comment : ''}</i>
 
-								<br>
-								<br>
+									<br>
+									<br>
 
-								{#if enhed.campaign}
-									<a href="/kontakt" class="btn primary-button-styling">Tag kontakt nu</a>
-								{/if}
+									{#if enhed.campaign}
+										<a href="/kontakt" class="btn primary-button-styling">Tag kontakt nu</a>
+									{/if}
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
+				{/if}
 			{/each}
 		{:catch error}
 			<p>Der skete en fejl, prøv igen</p>
