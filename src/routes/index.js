a4500e48c839ce52210cc26d4e307dfa22f91018
Henriette Steenhoff <henriette.steenhoff@protonmail.com>
2022-06-07 21:31:13 +0200

    Updated API version and CDN usage

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,8 @@
   const client = sanityClient({
 	projectId: "kmzqjgmb",
 	dataset: "production",
-	apiVersion: "2022-05-14",
-	useCdn: false
+	apiVersion: "2022-06-07",
+	useCdn: true
   });
 
   export { client }
